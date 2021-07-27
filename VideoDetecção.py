83f834255bae5083109d9910b1a0bf30d86d7253
japhysms <63021920+japhysms@users.noreply.github.com>
2021-07-27 05:26:01 -0300

    Create VideoDetecção.py

---
+++ VideoDetecção.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('2.mp4')
+cont = 0
+vez = 0
+while cap.isOpened():
+    cont = cont + 1
+    ret ,frame = cap.read()
+    if cv2.waitKey(1) == ord('q') or ret == False:
+        break
+    
+    frame = cv2.bilateralFilter(frame, 9, 75, 75)
+    
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    l_bound = np.array([0,130,150])
+    u_bound = np.array([35,250,255])
+    
+    mask = cv2.inRange(hsv, l_bound, u_bound)
+    
+    res = cv2.bitwise_and(frame, frame, mask= mask)
+    res_con = cv2.bitwise_and(frame, frame, mask= mask)
+    
+    gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    canny = cv2.Canny(gray, 100, 200)
+    
+    _,thr = cv2.threshold(mask, 100, 255, 0)
+    contours,_ = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(frame, contours, -1, (255,0,0), 3)
+
+    cv2.imshow('Original Frame', frame)
+    cv2.imshow('Mask', mask)
+    cv2.imshow('Res', res)
+    cv2.imshow('Res_con', res_con)
+    cv2.imshow('canny', canny)
+    if cont == 10:
+        cv2.imwrite('MascaraIMf5'+str(vez)+'.jpg',mask)
+        cv2.imwrite('Foto'+str(vez)+'.jpg',frame)
+        cont = 0
+        vez = vez + 1
+        if vez == 3:
+            break
+
+
+
+cap.release() 
+cv2.destroyAllWindows()
